c786bbea6a131cd16fb3ff293192cb9bef9e2385
carburetted <avtobaza@rocketmail.com>
2025-02-05 00:05:15 +0100

    Delete data_for_cluster.py

--- data_for_cluster.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Generate some sample data
-data = np.random.rand(100, 2)  # 100 data points with 2 features
-
-plt.scatter(data[:, 0], data[:, 1])
-plt.show()
