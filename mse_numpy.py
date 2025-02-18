02aeb5e07508501da984a30efa8056a44bdbd896
carburetted <avtobaza@rocketmail.com>
2025-02-18 22:59:31 +0100

    Delete mse_numpy.py

--- mse_numpy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def mean_squared_error(observed, predicted):
-    observed_np = np.array(observed)
-    predicted_np = np.array(predicted)
-    mse = np.mean((observed_np - predicted_np) ** 2)
-    return mse
-
-
